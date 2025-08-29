a205499d4a3e9b34117cae757629b8cf55209b71
DSRCODE <daljeetkalyanwat62@gmail.com>
2025-08-29 14:21:10 +0530

    code

--- src/pages/ManageServices/ManageServicesPage.jsx
+++ src/pages/ManageServices/ManageServicesPage.jsx
@@ -157,8 +157,8 @@ export default function ManageServicesPage() {
 
   return (
     <div className="max-w-7xl mx-auto p-8">
-      <div className="mb-8 flex flex-col md:flex-row md:items-center justify-between gap-4">
-        <h1 className="text-3xl font-bold text-center md:text-left text-black">
+      <div className="mb-8 flex flex-col md:flex-row md:items-center justify-between items-center gap-4">
+        <h1 className="text-2xl font-bold text-center md:text-left text-black">
           Manage Services
         </h1>
         <Button
@@ -169,7 +169,7 @@ export default function ManageServicesPage() {
             borderColor: "#6961AB",
             height: 44,
             fontWeight: 600,
-            fontSize: "1.05rem",
+            fontSize: "0.85rem",
           }}
           className="w-full md:w-auto"
           onClick={handleAddNew}
@@ -177,7 +177,7 @@ export default function ManageServicesPage() {
           Add New Service
         </Button>
       </div>
-      <h3 className="font-semibold text-lg mb-4 text-black">
+      <h3 className="font-semibold text-lg mb-1 px-2 text-gray-600">
         Your Current Services
       </h3>
       <div className="px-2 py-4 mb-4">
@@ -186,7 +186,7 @@ export default function ManageServicesPage() {
             {/* Category badge */}
             <div className="flex justify-start ">
               <div
-                className={`rounded-xl font-bold text-white text-base px-6 py-2 mb-4`}
+                className={`rounded-md font-medium text-white text-base px-8 py-1 mb-4`}
                 style={{
                   background:
                     service.category === "Hair"
@@ -202,7 +202,7 @@ export default function ManageServicesPage() {
             </div>
             {/* Service Title and Actions */}
             <div className="flex justify-between items-center mb-1">
-              <div className="font-bold text-xl text-black">{service.name}</div>
+              <div className="font-medium text-lg text-black">{service.name}</div>
               <div className="flex gap-2">
                 <Button
                   type="text"
